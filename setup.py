683065c66c781cd35081d20443e4f4c2fd734f89
notjustnonso <nonso.nwadialo@rice.edu>
2022-12-05 23:11:20 -0600

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A new template',
+    author='Nonso Nwadialo',
+    license='BSD-3',
+)
